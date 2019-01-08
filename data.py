ae5c79e46eadf28f66294016c873447c4fb6b5e7
rizkytm <ritrime97@gmail.com>
2019-01-08 15:00:14 +0700

    first commit

---
+++ data.py
@@ -0,0 +1,18 @@
+def Articles():
+	articles = [
+		{
+			'id': 1,
+			'title': 'Judul',
+			'body': 'Ini adalah isi',
+			'author': 'Rizky',
+			'create_date':'04-25-2017'
+		},
+		{
+			'id': 2,
+			'title': 'Judul lagi',
+			'body': 'Ini adalah isi lagi',
+			'author': 'Rizky Lagi',
+			'create_date':'04-26-2017'
+		}
+	]
+	return articles
